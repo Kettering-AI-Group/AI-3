ee2a1e61543ce4d48c2e54974225872ad324788c
Mitch Duncan <dunc0474@kettering.edu>
2016-02-28 22:38:31 -0500

    Implemented basic a-b method, provided framework for rest of methods
    A-B isn't perfect, but it's very close I think. The primary need is to
    implement a legal move generating function, and position evaluation
    function

--- BlackPlayer.java
+++ BlackPlayer.java
@@ -1,18 +1,14 @@
-
-
-
 public class BlackPlayer{
 // write this class.. Add variables and anything else you need
-         public BlackPlayer(String player_name, int boardSize, int maxTimePerMove){
-           //write this and all other methods
-         }
+   public BlackPlayer(String player_name, int boardSize, int maxTimePerMove){
+           
+   }
 //write this method and any other methods that you need
-         Move getMove(){
-
-               return null;
-         }
-          void update(Move m){
-		 
-		 }
+   Move getMove(){
+   
+      return null;
+   }
+   void update(Move m){
+   	 
+   }
 }
-
--- WhitePlayer.java
+++ WhitePlayer.java
@@ -1,16 +1,91 @@
-public class WhitePlayer{
+import java.util.*;
+public class WhitePlayer {
 // write this class.. Add variables and anything else you need
-
-         public WhitePlayer(String player_name, int boardSize, int maxTimePerMove){
+   public WhitePlayer(String player_name, int boardSize, int maxTimePerMove){
            
-         }
+   }
 //write this method and any other methods that you need
-         Move getMove(){
+   Move getMove(){
+   //alphaBeta(currentState, searchDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, true);
+      return null;
+   }		 
+   void update(Move m){
+   	 
+   }
 
-               return null;
+   private int[] alphaBeta(Node curNode, int searchDepth, int alpha, int beta, boolean maxPlayer) {
+      int[] tempVal = {0,0};//{curValue, locOfValue}
+      if(searchDepth == 0 || curNode.legalMoves.size() == 0) {
+         tempVal[0] = curNode.evalNode();
+         return tempVal;
+      }
+      if(maxPlayer == true) {
+         tempVal[0] = Integer.MIN_VALUE;
+         for(int i = 0; i < curNode.legalMoves.size(); i++) {        
+            tempVal[0] = Math.max(tempVal[0], alphaBeta(curNode.children.get(i), searchDepth - 1, alpha, beta, false)[0]);
+            tempVal[1] = i;
+            alpha = Math.max(alpha, tempVal[0]);
+            if(beta <= alpha) {
+               break;
+            }
          }
-		 
-		 void update(Move m){
-		 
-		 }
+         return tempVal;
+      } 
+      else {
+         tempVal[0] = Integer.MAX_VALUE;
+         for(int i = 0; i < curNode.legalMoves.size(); i++) {    
+            tempVal[0] = Math.min(tempVal[0], alphaBeta(curNode.children.get(i), searchDepth - 1, alpha, beta, true)[0]);
+            tempVal[1] = i;
+            beta = Math.min(beta, tempVal[0]);
+            if(beta <= alpha) {
+               break;
+            }
+         }
+         return tempVal;
+      }
+   }
+   
+   class Node {
+      Board board;
+      List<Node> children;
+      List<Move> legalMoves;
+   
+      protected Node(Board myBoard){
+         board = myBoard;
+         children = new ArrayList();
+         legalMoves = board.genMoves();
+      }
+      
+      private void genChild(Move move){
+         Board newBoard = board;
+         newBoard.applyMove(move);
+         Node child = new Node(newBoard);
+         children.add(child);
+      }
+      
+      private int evalNode(){
+         int result = -1;
+         return result;
+      }
+   
+   }
+   
+   class Board {
+      int[][] grid;
+   
+      protected Board(int boardSize){
+         grid = new int[boardSize][boardSize];
+      }
+      
+      protected List<Move> genMoves(){
+         List<Move> result = new ArrayList();
+         return result;
+      }
+      
+      protected void applyMove(Move move){
+         grid[move.getX1()][move.getY1()] = 2;
+         grid[move.getX2()][move.getY2()] = 2;
+         return;
+      }
+   }
 }
