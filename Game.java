7251874e2d6024cddeb9e2772740b2e959c0c600
Ivan <Ivan>
2016-03-06 12:35:05 -0500

    Fixed up the rest of the code to look somewhat structured and not so horrid to look at

--- Game.java
+++ Game.java
@@ -3,35 +3,39 @@ public class Game{
    final int BOARD_SIZE;
    int[][] connect6;
    public Game(int size){
-            BOARD_SIZE = size;
-            connect6= new int[BOARD_SIZE][BOARD_SIZE];
-            for (int i = 0; i < BOARD_SIZE; i++){
-                  for (int j = 0; j < BOARD_SIZE; j++){
-                           connect6[i][j] = 0;
-                  }
-            } 
-            connect6[BOARD_SIZE/2][BOARD_SIZE/2] = 1;
-         
-    }
+   BOARD_SIZE = size;
+   connect6= new int[BOARD_SIZE][BOARD_SIZE];
+   
+   for (int i = 0; i < BOARD_SIZE; i++){
+      for (int j = 0; j < BOARD_SIZE; j++){
+         connect6[i][j] = 0;
+      }
+   } 
+   
+   connect6[BOARD_SIZE/2][BOARD_SIZE/2] = 1;
+}
 
-      public void printBoard(){
-	   System.out.println(); 
-	    
-		    System.out.print("           " );
-			for (int j = 0; j < BOARD_SIZE; j++)
-			   System.out.print(j + "   ");
-			   System.out.println(); 
-			   System.out.println(); 
-            for (int i = 0; i < BOARD_SIZE; i++){
-			      System.out.print(i + ":         " );
-                  for (int j = 0; j < BOARD_SIZE; j++){
-                           System.out.print(connect6[i][j] + "   " ); 
-                  }
-                           System.out.println(); 
-            } 
-		System.out.println();
-		
-     }
+public void printBoard(){
+System.out.println(); 
+System.out.print("           " );
+
+for (int j = 0; j < BOARD_SIZE; j++)
+   System.out.print(j + "   ");
+   System.out.println(); 
+   System.out.println();
+    
+   for (int i = 0; i < BOARD_SIZE; i++){
+      System.out.print(i + ":         " );
+      
+      for (int j = 0; j < BOARD_SIZE; j++){
+         System.out.print(connect6[i][j] + "   " ); 
+      }
+      
+      System.out.println(); 
+   } 
+   
+   System.out.println();
+}
 
 
      public int update(Move m, String player){
